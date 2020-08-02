ba9866825d70b83e8acb9f042b4ddf58942775db
Jackson Melchert <melchert@stanford.edu>
2020-08-02 16:21:08 -0700

    Added peak wrpaper to peak_core

---
+++ peak_core/peak_wrapper.py
@@ -0,0 +1,161 @@
+from peak.family import *
+import hwtypes
+import inspect
+import os
+import logging
+
+from peak import Peak, family_closure, name_outputs
+from peak import family
+from peak.family import AbstractFamily
+from hwtypes import Tuple
+
+
+def exec_source(source, name, globals):
+    path = ".ast_tools"
+    file_name = os.path.join(path, f"{name}.py")
+    print(file_name)
+    os.makedirs(path, exist_ok=True)
+    with open(file_name, 'w') as fp:
+        fp.write(source)
+    try:
+        code = compile(source, filename=file_name, mode='exec')
+    except Exception as e:
+        logging.exception("Error compiling source")
+        raise e from None
+    _locals = {}
+    try:
+        exec(code, globals, _locals)
+    except Exception as e:
+        logging.exception("Error executing code")
+        raise e from None
+
+    #Need to write out a file, then exec using 'compile'
+    return _locals[name]
+
+def wrap_peak_class(PE_fc, Inst_fc):
+    inputs = PE_fc.Py.input_t
+    outputs = PE_fc.Py.output_t
+
+    new_inputs = {}
+
+    instr_name = 'inst'
+    instr_type = inputs.field_dict['inst']
+
+    new_inputs[instr_name] = inputs[instr_name]
+    new_inputs_str = "inst : Inst, "
+
+    tuple_inputs_str = ""
+    constructed_tuples = ""
+
+    pe_call_inputs = "inst, "
+
+    for name, type_ in inputs.field_dict.items(): 
+        if name != instr_name:
+
+            if issubclass(type_, hwtypes.Tuple):
+                tuple_inputs_str += "    " + name + "_tuple_t = Tuple["
+                pe_call_inputs += name + "_constructed, "
+                constructed_tuples += name + "_constructed = " + name + "_constructor(*["
+
+                for ind, i in enumerate(inputs[name]):
+                    if issubclass(i, hwtypes.BitVector):
+                        new_inputs[name + str(ind)] = "Data"
+                        new_inputs_str += name + str(ind) + " : Data, "
+                        tuple_inputs_str += "Data, "
+                        width = len(i)
+                    else:
+                        new_inputs[name + str(ind)] = "Bit"
+                        new_inputs_str += name + str(ind) + " : Bit, "
+                        tuple_inputs_str += "Bit, "
+
+                    constructed_tuples += name + str(ind) + ", "
+
+                tuple_inputs_str = tuple_inputs_str[:-2]
+                tuple_inputs_str += "]\n    "+name+"_constructor = family.get_constructor("+name+"_tuple_t)\n"
+                constructed_tuples = constructed_tuples[:-2]
+                constructed_tuples += "])\n            "
+            else:
+                if issubclass(type_, hwtypes.BitVector):
+                    new_inputs[name] = "Data"
+                    new_inputs_str += name + " : Data, "
+                    width = len(type_)
+                else:
+                    new_inputs[name] = "Bit"
+                    new_inputs_str += name + " : Bit, "
+                pe_call_inputs += name + ", "
+
+    pe_call_inputs = pe_call_inputs[:-2]
+    new_inputs_str = new_inputs_str[:-2]
+
+
+    outputs_str = ""
+    outputs_type_str = ""
+    new_outputs_str = ""
+    outputs_expanded = ""
+    named_outputs = "@name_outputs("
+
+    for idx, (name, type_) in enumerate(outputs.field_dict.items()): 
+        
+        if issubclass(type_, hwtypes.Tuple):
+            outputs_type_str += str(name) + ", "
+            
+            for ind, i in enumerate(outputs[name]):
+                if issubclass(i, hwtypes.BitVector):
+                    outputs_str += "Data, "
+                    out_width = len(i)
+                    named_outputs += f"{name}_{ind} = Data, "
+                else:
+                    outputs_str += "Bit, "
+                    named_outputs += f"{name}_{ind} = Bit, "
+                outputs_expanded += f"{name}[{ind}], "
+        elif issubclass(type_, hwtypes.BitVector):
+            outputs_str += "Data, "
+            outputs_type_str += str(name) + ", "
+            outputs_expanded += f"{name}, "
+            named_outputs += str(name) + " = Data, "
+        else:
+            outputs_str += "Bit, "
+            outputs_type_str += str(name) + ", "
+            outputs_expanded += f"{name}, "
+            named_outputs += str(name) + " = Bit, "
+            
+
+    named_outputs = named_outputs[:-2]
+    named_outputs += ")"
+    outputs_str = outputs_str[:-2]
+    outputs_type_str = outputs_type_str[:-2]
+    outputs_expanded = outputs_expanded[:-2]
+
+    new_peak_class = '''
+@family_closure
+def PE_wrapped_fc(family: AbstractFamily):
+    Data = family.BitVector['''+ str(width) +''']
+    Bit = family.Bit
+    Inst = Inst_fc(family)
+''' + tuple_inputs_str + '''
+    @family.assemble(locals(), globals())
+    class PE_wrapped(Peak):
+        def __init__(self):
+            self.PE : PE_fc(family) = PE_fc(family)()
+        '''+named_outputs+'''
+        def __call__(self, ''' + new_inputs_str + ''') -> (''' + outputs_str + '''):
+            ''' + constructed_tuples + '''
+            ''' + outputs_type_str + ''' = self.PE(''' + pe_call_inputs + ''')
+            return ''' + outputs_expanded + '''
+    
+    return PE_wrapped
+'''
+    print(new_peak_class)
+    _globals = dict(
+        Peak=Peak,
+        AbstractFamily=AbstractFamily,
+        family_closure=family_closure,
+        name_outputs=name_outputs,
+        family=family,
+        Inst_fc=Inst_fc,
+        # Inst=Inst,
+        PE_fc=PE_fc,
+        Tuple=Tuple
+    )
+
+    return exec_source(new_peak_class, "PE_wrapped_fc", _globals)
--- tests/test_peak_core/test_new_pe.py
+++ tests/test_peak_core/test_new_pe.py
@@ -1,7 +1,7 @@
 from gemstone.common.testers import BasicTester
 from gemstone.common.run_verilog_sim import irun_available
 from peak_core.peak_core import PeakCore
-from peak.peak_wrapper import wrap_peak_class
+from peak_core.peak_wrapper import wrap_peak_class
 from peak.family import PyFamily
 
 import hwtypes
@@ -10,76 +10,19 @@ import tempfile
 import os
 import pytest
 
-
 from peak import Peak, family_closure, name_outputs, family
 from functools import lru_cache
 import magma as m
 from hwtypes.adt import Tuple
-from lassen.sim import PE_fc, Inst_fc
-
-from lassen.asm import (add, Mode_t, lut_and, inst, ALU_t, Cond_t, 
-                        umult0, fp_mul, fp_add,
-                        fcnvexp2f, fcnvsint2f, fcnvuint2f)
-
 
 from peak_gen.sim import pe_arch_closure
 from peak_gen.isa import inst_arch_closure
 from peak_gen.arch import read_arch
 from peak_gen.asm import asm_arch_closure
 
-@pytest.fixture(scope="module")
-def dw_files():
-    filenames = ["DW_fp_add.v", "DW_fp_mult.v"]
-    dirname = "peak_core"
-    result_filenames = []
-    for name in filenames:
-        filename = os.path.join(dirname, name)
-        assert os.path.isfile(filename)
-        result_filenames.append(filename)
-    return result_filenames
-
-def test_new_pe(dw_files):
-
-    # PE_wrapped_fc = wrap_peak_class(PE_fc, Inst_fc)
-
-    core = PeakCore(PE_fc)
-    core.name = lambda: "PECore"
-    circuit = core.circuit()
-
-    # random test stuff
-    tester = BasicTester(circuit, circuit.clk, circuit.reset)
-    tester.reset()
-
-    tester.poke(circuit.interface["stall"], 0)
-    config_data = core.get_config_bitstream(add())
-
-    for addr, data in config_data:
-        print("{0:08X} {1:08X}".format(addr, data))
-        tester.configure(addr, data)
-        tester.config_read(addr)
-        tester.eval()
-
-    for i in range(10):
-        tester.poke(circuit.interface["data0"], 0x42)
-        tester.poke(circuit.interface["data1"], 0x42)
-        tester.eval()
-        tester.print("O=%d\n", circuit.interface["alu_res_0"])
-        tester.expect(circuit.interface["alu_res_0"], 0x42 + 0x42)
-
-
-    with tempfile.TemporaryDirectory() as tempdir:
-        for filename in dw_files:
-            shutil.copy(filename, tempdir)
-        tester.compile_and_run(target="verilator",
-                               magma_output="coreir-verilog",
-                               magma_opts={"coreir_libs": {"float_DW"}},
-                               directory=tempdir,
-                               flags=["-Wno-fatal"])
-
-
 def test_peak_generator():
 
-    arch = read_arch(str("../../peak_generator/examples/misc_tests/test_add.json"))
+    arch = read_arch(str("../../peak_generator/examples/misc_tests/lassen.json"))
     PE_fc = pe_arch_closure(arch)
     Inst_fc = inst_arch_closure(arch)
 
@@ -92,7 +35,6 @@ def test_peak_generator():
     core.name = lambda: "PECore"
     circuit = core.circuit()
 
-    # random test stuff
     tester = BasicTester(circuit, circuit.clk, circuit.reset)
     tester.reset()
 
@@ -109,15 +51,13 @@ def test_peak_generator():
         tester.poke(circuit.interface["inputs0"], 0x42)
         tester.poke(circuit.interface["inputs1"], 0x42)
         tester.eval()
-        tester.print("O=%d\n", circuit.interface["PE_res_0"])
-        tester.expect(circuit.interface["PE_res_0"], 0x42 + 0x42)
+        tester.print("O=%d\n", circuit.interface["pe_outputs_0"])
+        tester.expect(circuit.interface["pe_outputs_0"], 0x42 + 0x42)
 
 
     with tempfile.TemporaryDirectory() as tempdir:
-        # for filename in dw_files:
-        #     shutil.copy(filename, tempdir)
+
         tester.compile_and_run(target="verilator",
                                magma_output="coreir-verilog",
-                            #    magma_opts={"coreir_libs": {"float_DW"}},
                                directory=tempdir,
                                flags=["-Wno-fatal"])
