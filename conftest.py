f2b6723ecda21a5efdd59c14818b18a5ebb22eed
Lenny Truong <lenny@stanford.edu>
2020-08-12 05:12:20 -0700

    Try gc collect

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from magma import clear_cachedFunctions
 import magma
 from gemstone.generator import clear_generator_cache
 from coreir.lib import libcoreir_c
+import gc
 
 collect_ignore = [
     # TODO(rsetaluri): Remove this once it is moved to canal!
@@ -17,17 +18,12 @@ collect_ignore = [
 def magma_test():
     clear_cachedFunctions()
     ctx = magma.backend.coreir_.CoreIRContextSingleton().get_instance()
+    # Remove reference
     if ctx in magma.backend.coreir_._context_to_modules:
         del magma.backend.coreir_._context_to_modules[ctx]
-    # Set flag so __del__ doesn't free twice
-    ctx.external_ptr = True
-    # Force memory free to avoid memory leak issue, see
-    # https://github.com/rdaly525/coreir/issues/896
-    # and https://github.com/StanfordAHA/garnet/pull/638
-    # TODO: We should add a cleaner API for this to magma/pycoreir and update
-    # this code accordingly
-    libcoreir_c.COREDeleteContext(ctx.context)
     magma.frontend.coreir_.ResetCoreIR()
+    # Force garbage collection so CoreIR context is freed
+    gc.collect()
     clear_generator_cache()
 
 
