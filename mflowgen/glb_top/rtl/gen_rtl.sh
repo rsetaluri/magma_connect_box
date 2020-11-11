e8ca9eef1da119e8df73fbe32e1797c715e820bd
Taeyoung Kong <kongty@stanford.edu>
2020-11-10 21:26:44 -0800

    Delete make clean

--- mflowgen/glb_top/rtl/gen_rtl.sh
+++ mflowgen/glb_top/rtl/gen_rtl.sh
@@ -6,11 +6,10 @@ if [ -f ../inputs/design.v ]; then
   (cd outputs; ln -s ../../inputs/design.v)
 else
   # SystemRDL run
-  make clean
   make -C $GARNET_HOME/global_buffer rtl CGRA_WIDTH=${cgra_width} NUM_GLB_TILES=${num_glb_tiles}
-  
+
   rm -f outputs/design.v
-  
+
   while read F  ; do
       echo "Reading design file: $F"
       cat $GARNET_HOME/global_buffer/rtl/$F >> outputs/design.v
