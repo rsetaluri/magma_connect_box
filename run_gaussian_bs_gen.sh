a7d61272eeebdf29498b575f60b57b2e16785a5c
Jackson Melchert <melchert@stanford.edu>
2021-04-07 09:55:26 -0700

    Added bs gen script for gaussian

---
+++ run_gaussian_bs_gen.sh
@@ -0,0 +1,1 @@
+python garnet.py --no-pd --interconnect-only --input-app gaussian/gaussian_to_metamapper.json --input-file gaussian/input.raw --output-file gaussian/gaussian.bs --gold-file gaussian/gold.raw
