6be626e67e1a9884bbbd298965abd8da74e5b440
norabarlow <kathleen.feng0@gmail.com>
2021-04-12 23:56:38 -0700

    clone garnet repo to avoid rtl clashes

--- run.sh
+++ run.sh
@@ -2,20 +2,27 @@
 
 export APP=$1
 export DSE_PE=$2
-export HEIGHT=8
-export WIDTH=8
+export HEIGHT=$3
+export WIDTH=$4
 export CLK=1c1
 
-home=$PWD
+home=$PWD/mflowgen
 
-cd mflowgen/Tile_PE
+cd mflowgen
 build_dir=build_${DSE_PE}_${APP}_${CLK}
 mkdir ${build_dir}
-mflowgen run --design ../
+cd ${build_dir}
+git clone --branch pe-dse-power https://github.com/StanfordAHA/garnet.git
+
+mkdir Tile_PE
+cd Tile_PE
+mflowgen run --design ../garnet/mflowgen/Tile_PE
 make post-synth-power
-cd ../Tile_MemCore
-mkdir ${build_dir}
-mflowgen run --design ../
+
+cd ../
+mkdir Tile_MemCore
+cd Tile_MemCore
+mflowgen run --design ../garnet/mflowgen/Tile_MemCore
 mflowgen stash link --path /sim/kzf/pe-dse/stash/2021-0409-mflowgen-stash-1b421f
 mflowgen stash pull --hash 2aec61
 make post-synth-power
@@ -24,5 +31,5 @@ cd ${home}
 results_dir=../micro-2021/pe-dse-power/micro2021/results/${DSE_PE}/${APP}/${CLK}
 for t in Tile_PE Tile_MemCore; do
     mkdir -p ${results_dir}/${t}
-    cp mflowgen/${t}/${build_dir}/*-post-synth-power/outputs/reports/* ${results_dir}/${t}
+    cp mflowgen/${build_dir}/garnet/${t}/*-post-synth-power/outputs/reports/* ${results_dir}/${t}
 done
